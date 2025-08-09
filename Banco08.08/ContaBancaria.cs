e00994835aee81302696a882065d17d9c43b5acb
Rafael Mariano <rafael.m.mariano@aluno.senai.br>
2025-08-08 22:53:54 -0300

    Update ContaBancaria.cs

--- Banco08.08/ContaBancaria.cs
+++ Banco08.08/ContaBancaria.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 //Aqui temos a class ContaBancaria,separei em 3 funções depositar onde vão botar dinheiro tendo a ideia q o deposito tem q ser maior que $1 usando o if q tem q ser maior q 1 e uso atribuição += soma
 //no sacar uso if para impor um limite q só pode sacar se tiver saldo q seja maior q 0 e uso o -= para tirar tirar dinheiro ,ele subtrai
 // e por fim o void mostrar dados,faço ali para mostrar o texto/informações final q mostrará pro cliente do banco e no final só pra chamar na program e rodar no while
+
+//fiz desse jeito pq é o jeito q vejo mais facil de entendimento e é o jeito q foi me ensinado a ordem dos metodos para mlr organização
 namespace BancoDosProgramadores
 {
     public class ContaBancaria
@@ -64,4 +66,5 @@ namespace BancoDosProgramadores
         }
     }
 }
+
 
